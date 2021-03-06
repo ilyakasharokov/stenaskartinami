48ad67edfa0fb4d63caf37e6b8940f5b747a8632
ilya <ilyakasharokov@mail.ru>
2021-03-06 14:45:24 +0000

    medium

--- api/api/art/models/art.js
+++ api/api/art/models/art.js
@@ -13,12 +13,33 @@ module.exports = {
       if (data.Title) {
         data.slug = slugify(data.Title, {lower: true,remove: /[*+~.()'"!:@ь]/g});
       }
+      if(data.width && data.height){
+        data.square = data.width * data.height;
+      }
+      if(data.width === data.height){
+        data.isSquare = true;
+      }
     },
     beforeUpdate: async (params, data) => {
         console.log('before' + data.Title)
       if (data.Title) {
         data.slug = slugify(data.Title, {lower: true,remove: /[*+~.()'"!:@ь]/g});
       }
+      if(data.Size){
+        if(data.Size.Height){
+          data.height = data.Size.Height
+        }
+        if(data.Size.Width){
+          data.width = data.Size.Width
+        }
+        if(data.width && data.height){
+          data.square = data.width * data.height;
+        }
+        if(data.width === data.height){
+          data.isSquare = true;
+        }
+      }
     },
   },
 };
+
