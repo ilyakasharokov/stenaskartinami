37702a42c7bc5bb10fbd2ce7807d1a014749edf9
ikasharokov <ikasharokov>
2021-03-15 01:24:27 +0300

    filters

--- front/components/catalog/catalog-filters.js
+++ front/components/catalog/catalog-filters.js
@@ -5,26 +5,34 @@ import { useRouter } from "next/router";
 import Router from 'next/router'
 import Preloader from '../preloader/preloader';
 
-
+const FILTER_ITEMS_NUM = 6;
 
 export default function CatalogFilters({arts, onChange}){
 
   const [filters, setFilters] = useState({
     styles:{
       title: 'Стиль',
-      items: []
+      items: [],
+      open: true,
+      showAll: false,
     }, 
     subjects: {
       title: 'Теги',
-      items: []
+      items: [],
+      open: true,
+      showAll: false,
     }, 
       mediums: {
       title: 'Техника',
-      items: []
+      items: [],
+      open: false,
+      showAll: false,
     }, 
     size: {
       title: 'Размер',
-      items: []
+      items: [],
+      open: false,
+      showAll: false,
     },  
 
   })
@@ -74,6 +82,7 @@ export default function CatalogFilters({arts, onChange}){
                 return item.slug === queryItem;
               }) > -1 || Router.query[key] === item.slug){
                 item.active = true;
+                newFilters[key].open = true;
                 newFilters[key].activeCount ++;
               }
             });
@@ -91,6 +100,18 @@ export default function CatalogFilters({arts, onChange}){
     arr.sort((a,b)=> a.active === b.active ? 0: a.active ? -1: 1)
   }
 
+  function showAll(key){
+    let newFilters = Object.assign({}, filters)
+    newFilters[key].showAll = true;
+    setFilters(newFilters);
+  }
+
+  function toggleCollapse(key){
+    let newFilters = Object.assign({}, filters)
+    newFilters[key].open = !newFilters[key].open;
+    setFilters(newFilters);
+  }
+
   function сheckboxClick(item, type){
     item.active = !item.active;
     // sortByActive(filters[type])
@@ -103,16 +124,29 @@ export default function CatalogFilters({arts, onChange}){
       pathname: Router.pathname,
       query: Router.query ? Object.assign({}, Router.query, query) : query
     })
-  }
+  } 
 
   return (
     <div className="catalog-filters">
       {
         Object.keys(filters).map((key) => 
           <div className="catalog-filters__section" key={key}>
-            <div className="catalog-filters__section-title">{filters[key].title}</div> 
+            <div className="catalog-filters__section-top">
+              <div className="catalog-filters__section-title">{filters[key].title}</div> 
+              <div className="catalog-filters__section-expand-btn" onClick={ ()=> toggleCollapse(key)}>
+              {
+                filters[key].open && 
+                <svg className="minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 1"><path d="M0 0h10v1H0V0z" fill="#333"></path></svg>
+              }
+              {
+                !filters[key].open && 
+                <svg viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg"><g fill="#333" fillRule="evenodd"><path d="M0 6h13v1H0z"></path><path d="M6 0h1v13H6z"></path></g></svg>
+              }
+              </div> 
+            </div> 
+            <div className="catalog-filters__collapsable" style={ { maxHeight: (filters[key].open && ((!filters[key].showAll && FILTER_ITEMS_NUM || filters[key].items.length ) + 1) * 25) || 0 + 'px'  }} length={!filters[key].showAll && FILTER_ITEMS_NUM || filters[key].items.length}>
             {
-              filters[key].items && filters[key].items.map( (item) => 
+              (!filters[key].showAll && filters[key].items && filters[key].items.slice(0, FILTER_ITEMS_NUM) || filters[key].items && filters[key].items).map( (item) => 
                 <div className="catalog-filters__item" key={item.id}>
                   <div className={`checkbox ${item.active ? "checkbox--active": ""}`} onClick={()=>сheckboxClick(item, key)}></div>
                   <div>{ item.Title || item.title }</div>
@@ -120,10 +154,15 @@ export default function CatalogFilters({arts, onChange}){
               )
             }
             {
-              !filters[key].items.length &&
+              !filters[key].showAll && filters[key].items.length > FILTER_ITEMS_NUM &&
+              <div className="catalog-filters__show-all" onClick={ ()=> showAll(key) }>Показать все</div>
+            }
+            </div>
+            {
+              /* !filters[key].items.length &&
               <div className="catalog-filters__preloader">
                 <Preloader></Preloader>
-              </div>
+              </div> */
             }
           </div>
         )
