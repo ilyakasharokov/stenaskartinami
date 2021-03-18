4d35e3e705430feb90fdf3ac9a68390d0aa503c6
ikasharokov <ikasharokov>
2021-03-18 15:26:46 +0300

    mailgun

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
   email: {
     provider: 'mailgun',
     providerOptions: {
-      apiKey: env('MAILGUN_API_KEY')
+      apiKey: env('MAILGUN_API_KEY'),
+      domain: env('MAILGUN_YOUR_DOMAIN')
     },
     settings: {
       defaultFrom: 'no-reply@stenaskartinami.com',
