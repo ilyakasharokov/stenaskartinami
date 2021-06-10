7f0425c35d772310c23ba99974b16216dde3adfb
ilya <ilyakasharokov@mail.ru>
2021-06-10 07:39:42 +0000

    c

--- api/api/form/controllers/form.js
+++ api/api/form/controllers/form.js
@@ -27,7 +27,7 @@ module.exports = {
       await strapi.plugins['email'].services.email.send({
         to: 'ilyakasharokov@mail.ru, dudkinet@gmail.com',
         from: 'no-reply@stenaskartinami.com',
-        subject: 'На вас написали заявление',
+        subject: 'Стена с картинами, заявка YO',
         text: `
 	  Форма: ${entity.title}
 	  Имя: ${entity.name}
