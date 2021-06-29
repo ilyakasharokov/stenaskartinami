3f3aefd2147b190288018121eb60d3b96750dd4b
ikasharokov <ikasharokov>
2021-06-29 17:29:15 +0300

    callback url

--- front/pages/auth/signin.js
+++ front/pages/auth/signin.js
@@ -1,27 +1,36 @@
 import { getProviders, signIn } from 'next-auth/client'
 import Head from 'next/head'
+import { useSession } from "next-auth/client";
+import Router from 'next/router'
 
 export default function SignIn({ providers }) {
-  return (
-    <>
-        <Head>
-            <title>Авторизация | Стена с картинами</title>
-        </Head>
-        <div className="login-page__wrapper">
-            <div className="login-page">
-                <div className="login-page__logo">
-                    <img src="/images/Logo_stenaskartinami_black.svg"/> 
-                    <div className="login-page__text">Стена с картинами</div>
-                </div>
-                {Object.values(providers).map(provider => (
-                    <div key={provider.name}>
-                    <button onClick={() => signIn(provider.id)}>Войти через {provider.name}</button>
+
+    const [session, loading] = useSession();
+
+    if(session){
+        Router.push('/')
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Авторизация | Стена с картинами</title>
+            </Head>
+            <div className="login-page__wrapper">
+                <div className="login-page">
+                    <div className="login-page__logo">
+                        <img src="/images/Logo_stenaskartinami_black.svg"/> 
+                        <div className="login-page__text">Стена с картинами</div>
                     </div>
-                ))}
+                    {Object.values(providers).map(provider => (
+                        <div key={provider.name}>
+                        <button onClick={() => signIn(provider.id)}>Войти через {provider.name}</button>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 // This is the recommended way for Next.js 9.3 or newer
