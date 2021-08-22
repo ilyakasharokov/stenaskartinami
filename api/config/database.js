4b170748503a692b11f39a613a85fcff00c1cd1b
ikasharokov <ikasharokov>
2021-08-22 13:01:43 +0300

    buld

--- api/config/database.js
+++ api/config/database.js
@@ -1,21 +1,21 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
     connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        client: 'mongo',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+      default: {
+        connector: 'mongoose',
+        settings: {
+          client: 'mongo',
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 27017),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', null),
+          password: env('DATABASE_PASSWORD', null),
+        },
+        options: {
+          authenticationDatabase: env('AUTHENTICATION_DATABASE'),
+          ssl: env('DATABASE_SSL'),
+        },
       },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE'),
-        ssl: env('DATABASE_SSL'),
-      },
-    },
   }, 
   /* connections: {
     default: {
@@ -28,5 +28,5 @@ module.exports = ({ env }) => ({
         useNullAsDefault: true,
       },
     },
-  }*/
+  } */
 });
